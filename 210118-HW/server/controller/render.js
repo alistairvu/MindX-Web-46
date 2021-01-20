979ed7f10527fd10d162b1af84a7435406689494
Alistair Vu <alistairvu@gmail.com>
2021-01-20 22:37:47 +0700

    Streamline code

--- 210118-HW/server/controller/render.js
+++ 210118-HW/server/controller/render.js
@@ -2,13 +2,11 @@ const path = require("path")
 // @desc    Load main page
 // @param   GET /
 const loadMain = (request, response) => {
-  response.status(200)
   response.sendFile(path.resolve(__dirname, "../../public/main/index.html"))
 }
 // @desc    Load ask page
 // @param   GET /ask
 const loadAsk = (request, response) => {
-  response.status(200)
   response.sendFile(path.resolve(__dirname, "../../public/ask/index.html"))
 }
 
