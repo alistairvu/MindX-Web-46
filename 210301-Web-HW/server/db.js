cfba5433c08386fbf8db2ea75bb65995d41c19c4
Alistair Vu <alistairvu@gmail.com>
2021-03-05 13:48:24 +0700

    Add Mongo connection link

--- 210301-Web-HW/server/db.js
+++ 210301-Web-HW/server/db.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose")
 const connectDB = async () => {
   try {
     console.log(process.env.MONGO_URI)
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    })
+    const conn = await mongoose.connect(
+      "mongodb+srv://admin_user:drcwonWhIMVzam9A@cluster0.0ltia.mongodb.net/votes?retryWrites=true&w=majority",
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      }
+    )
     console.log(`MongoDB Connected: ${conn.connection.host}`)
   } catch (error) {
     console.error(`Error: ${error.message}`)
