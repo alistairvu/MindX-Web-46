2f653b1553e1cc0e6efbfe3d0864b27491c0e8f8
Alistair Vu <alistairvu@gmail.com>
2021-04-01 11:06:02 +0700

    Add populate to comment controller

--- mindx-images/backend/modules/comment/comment.controller.ts
+++ mindx-images/backend/modules/comment/comment.controller.ts
@@ -8,7 +8,7 @@ import HTTPError from "../../httpError"
 export const getComments = async (req: Request, res: Response, next: any) => {
   try {
     const postId = req.params.id
-    const post = await Post.findById(postId)
+    const post = await Post.findById(postId).populate("createdBy")
 
     if (!post) {
       throw new HTTPError("No matching posts found", 404)
